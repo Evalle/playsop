0b617428245e2cde0a04f9369d62ca7e62d1aa5a
evalle <shmarnev@gmail.com>
2016-10-24 13:22:54 +0200

    Add some sanity checking to the script

--- playsop.sh
+++ playsop.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
+# current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PLAYERTV="mpv --quiet --idle=yes --audio-device=pulse/alsa_output.pci-0000_00_03.0.hdmi-stereo"
+PLAYERNB="mpv --quiet --idle=yes"
 
-PLAYER="mpv --quiet --idle=yes"
-
-URL=$1
-
-# TO-DO - check that sp-sc-auth exists
-
-./sp-sc-auth $URL 3908 8908 >/dev/null &
+./sp-sc-auth sop://broker.sopcast.com:3912/$1 3908 8908 &
 
 _PID=$!
 
-cleanup () {
-    ps | grep -q $_PID && kill $_PID
-    exit
-}
-
-trap cleanup SIGINT SIGTERM EXIT
-
-# Wait up to 10 seconds to connect
 n=0
 until [ $n -ge 10 ]; do
-    # if sp-auth died, exit
+    # is sp-auth died, exit
     ps | grep -q $_PID || exit 1
 
-    netstat -vant | grep 8908 | grep -q LISTEN && break
-    n=$[$n+1]
+    netstat -vant | grep 8908 | grep -q LISTEN  && break
+    n=$[n+1]
     sleep 1
 done
 
-$PLAYER http://localhost:8908/tv.asf
+$PLAYERTV http://localhost:8908/tv.asf
